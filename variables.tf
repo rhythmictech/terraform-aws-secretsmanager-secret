1a2a6fe6917c446bc17aa5dcecc7f48128323076
Steven Black <genseb7@gmail.com>
2020-05-27 16:51:11 -0400

    alphabet eyes

--- variables.tf
+++ variables.tf
@@ -1,32 +1,31 @@
-# Variables
+
+variable "description" {
+  default = "terraform-managed secret"
+  type    = string
+}
+
+variable "kms_key_id" {
+  default     = null
+  description = "Optional. The KMS Key ID to encrypt the secret. KMS key arn or alias can be used."
+}
+
+variable "policy" {
+  default     = null
+  description = "Optional. The resource policy which controls access to the secret."
+}
 
 variable "name" {
   description = "Name of secret to store"
   type        = string
 }
 
+variable "tags" {
+  default     = {}
+  description = "User-Defined tags"
+  type        = map(string)
+}
+
 variable "value" {
   description = "Secret value to store"
   type        = string
-}
-
-variable "description" {
-  type    = string
-  default = "terraform-managed secret"
-}
-
-variable "tags" {
-  description = "User-Defined tags"
-  type        = map(string)
-  default     = {}
-}
-
-variable "policy" {
-  description = "Optional. The resource policy which controls access to the secret."
-  default     = null
-}
-
-variable "kms_key_id" {
-  description = "Optional. The KMS Key ID to encrypt the secret. KMS key arn or alias can be used."
-  default     = null
 }
