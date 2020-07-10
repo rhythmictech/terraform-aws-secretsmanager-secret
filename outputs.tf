c650e1a43c34186b2ea2372bd6d568c600fe89f7
Steven Black <genseb7@gmail.com>
2020-07-09 21:15:14 -0400

    fix #8 add arn

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
 
+output "arn" {
+  description = "AWS SecretManager Secret ARN"
+  value       = aws_secretsmanager_secret.secret.arn
+}
+
+output "id" {
+  description = "AWS SecretManager Secret ARN"
+  value       = aws_secretsmanager_secret.secret.id
+}
+
 output "secret" {
   description = "AWS SecretManager Secret resource"
   value       = aws_secretsmanager_secret.secret
